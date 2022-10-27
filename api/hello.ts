7fbfbbe844f685a9e6bae4cc3598e58a183f3da6
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-27 19:57:20 +0200

    knexfix

--- api/hello.ts
+++ api/hello.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function (req: VercelRequest, res: VercelResponse) {
+export default function (req: VercelRequest, res: VercelResponse): void {
   const { name = 'World' } = req.query
   res.send(`Hello ${name}!`)
 }
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,6 @@
-const { loadEnvConfig } = require('@next/env')
-
-const dev = process.env.NODE_ENV !== 'production'
-const { PG_URI } = loadEnvConfig('./', dev).combinedEnv
-
 module.exports = {
   client: 'pg',
-  connection: PG_URI,
+  connection: process.env.PG_URI,
   migrations: {
     directory: './migrations',
   },
