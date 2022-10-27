9c286737b93a0e8b7cd839516fbefacef5657cc3
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-27 21:42:32 +0200

    matches

---
+++ api/matches.ts
@@ -0,0 +1,23 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import knex from 'knex'
+
+import config from '../knexfile.js'
+
+export default async function (req: VercelRequest, res: VercelResponse): Promise<void> {
+  const knexen = knex(config)
+  const matches = (
+    await knexen.raw(
+      `
+          SELECT m.game_start,
+                 m.away_team,
+                 m.home_team,
+                 m.channel
+          FROM bets b,
+               matches m
+          ORDER BY game_start, m.id asc;`
+    )
+  ).rows
+  knexen.destroy()
+
+  res.json(matches)
+}
