aca5378d9e62642b55ee3122bb49f2d3332c924f
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-10-29 23:56:09 +0200

    update bet api

--- api/v1/me/bets/[id].ts
+++ api/v1/me/bets/[id].ts
@@ -3,9 +3,18 @@ import { auth } from '../../../../auth/authHandler'
 import { ApiHandlerOpts } from '../../../../types/apiHandlerOpts'
 
 const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
-    const { res, req } = opts
+    const { res, req, knex, user } = opts
     const { id } = req.query
-
-    res.status(200).json({ req: JSON.parse(req.body), id })
+    if (!user) {
+        res.status(401)
+        return
+    }
+    const reqBody = JSON.parse(req.body)
+    const bet = {
+        home_score: reqBody.home_score,
+        away_score: reqBody.away_score,
+    }
+    await knex('bets').where('user_id', '=', user.id).where('id', '=', id).update(bet)
+    res.status(200).json(bet)
 }
 export default allowCors(auth(handler))
