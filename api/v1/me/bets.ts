215695625201ede49569e81573d51370d5613f4e
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-11-10 16:17:44 +0100

    bytt med pg i bets

--- api/v1/me/bets.ts
+++ api/v1/me/bets.ts
@@ -1,12 +1,17 @@
 import { allowCors } from '../../../cors/corsHelper'
-import { auth } from '../../../auth/authHandler'
-import { ApiHandlerOpts } from '../../../types/apiHandlerOpts'
+import { auth } from '../../../auth/authHandlerPg'
+import { ApiHandlerOptsPg } from '../../../types/apiHandlerOptsv2'
 
-const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
-    const { user, res, knex } = opts
+const handler = async function handler(opts: ApiHandlerOptsPg): Promise<void> {
+    const { user, res, client } = opts
+
+    if (!user) {
+        res.status(401)
+        return
+    }
 
     const upcoming = (
-        await knex.raw(
+        await client.query(
             `
           SELECT m.game_start,
                  m.away_team,
