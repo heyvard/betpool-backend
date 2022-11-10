cc2da757ff84427ec9a7a37ad647a78137c43250
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-11-10 15:38:22 +0100

    PG chat

--- api/v1/chat.ts
+++ api/v1/chat.ts
@@ -1,9 +1,9 @@
 import { allowCors } from '../../cors/corsHelper'
-import { auth } from '../../auth/authHandler'
-import { ApiHandlerOpts } from '../../types/apiHandlerOpts'
+import { auth } from '../../auth/authHandlerPg'
+import { ApiHandlerOptsPg } from '../../types/apiHandlerOptsv2'
 
-const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
-    const { res, req, user, knex } = opts
+const handler = async function handler(opts: ApiHandlerOptsPg): Promise<void> {
+    const { res, req, user, client } = opts
     if (!user) {
         res.status(401)
         return
@@ -11,24 +11,22 @@ const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
     if (req.method == 'POST') {
         const reqBody = JSON.parse(req.body)
 
-        const data = {
-            message: reqBody.message,
-            user_id: user.id,
-        }
-        await knex('chat').insert(data)
-        res.status(201).json(data)
+        await client.query('INSERT INTO chat (message, user_id) VALUES ($1, $2)', [reqBody.message, user.id])
+        res.status(201).json({ ok: ':)' })
         return
     }
 
     const chat = (
-        await knex.raw(`
+        await client.query(`
         SELECT u.id userid,
                u.name,
                u.picture,
                c.*
         FROM chat c,
              users u
-        WHERE c.user_id = u.id`)
+        WHERE c.user_id = u.id
+        ORDER BY c.created_at asc 
+        `)
     ).rows
 
     res.json(chat)
