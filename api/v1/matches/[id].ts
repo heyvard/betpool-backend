06e12e61788c5bbecdc0553e7140fccf1508545a
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-11-21 17:07:10 +0100

    match api

--- api/v1/matches/[id].ts
+++ api/v1/matches/[id].ts
@@ -1,6 +1,6 @@
-import { allowCors } from '../../../../cors/corsHelper'
-import { auth } from '../../../../auth/authHandler'
-import { ApiHandlerOpts } from '../../../../types/apiHandlerOpts'
+import { allowCors } from '../../../cors/corsHelper'
+import { auth } from '../../../auth/authHandler'
+import { ApiHandlerOpts } from '../../../types/apiHandlerOpts'
 
 const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
     const { user, res, req, client } = opts
@@ -10,22 +10,23 @@ const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
         return
     }
 
+    if (!user.admin) {
+        res.status(403)
+        return
+    }
+
     const { id } = req.query
 
     const reqBody = JSON.parse(req.body)
 
     await client.query(
         `
-        UPDATE bets
+        UPDATE matches
         SET home_score = $1,
             away_score = $2
-        WHERE user_id = $3
-          AND id = $4
-          AND match_id in (select id
-                           from matches
-                           WHERE game_start > now());
+        WHERE  id = $3;
     `,
-        [reqBody.home_score, reqBody.away_score, user.id, id],
+        [reqBody.home_score, reqBody.away_score, id],
     )
     res.status(200).json({ ok: 123 })
 }
