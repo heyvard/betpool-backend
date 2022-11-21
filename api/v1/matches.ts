f98d7691f5a9c4ad62dce5deee6bd13a462500c0
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-11-21 17:13:21 +0100

    matches api

---
+++ api/v1/matches.ts
@@ -0,0 +1,22 @@
+import { allowCors } from '../../cors/corsHelper'
+import { ApiHandlerOpts } from '../../types/apiHandlerOpts'
+import { auth } from '../../auth/authHandler'
+
+const handler = async function handler(opts: ApiHandlerOpts): Promise<void> {
+    const { res, user, client } = opts
+    if (!user) {
+        res.status(401)
+        return
+    }
+
+    const matches = (
+        await client.query(
+            `
+          SELECT *
+          FROM  matches m;`,
+        )
+    ).rows
+    res.status(200).json(matches)
+}
+
+export default allowCors(auth(handler))
